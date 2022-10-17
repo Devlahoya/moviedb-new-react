80cacef91bf62d094be3e5bba8939de80ccadf06
Devlahoya <devlahoya@gmail.com>
2022-10-17 11:44:24 -0500

    First upload

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import 'swiper/swiper.min.css';
+import './assets/boxicons-2.0.7/css/boxicons.min.css';
+import './App.scss';
+
+import { Routes, Route } from 'react-router-dom';
+
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+
+import Home from './pages/Home';
+import Catalog from './pages/Catalog';
+import Detail from './pages/detail/Detail';
+
+
+function App() {
+    return (
+        <>
+        <Header />
+        <Routes>
+        <Route
+                path='/:category/search/:keyword'
+                element={<Catalog/>}
+            />
+            <Route
+                path='/:category/:id'
+                element={<Detail/>}
+            />
+            <Route
+                path='/:category'
+                element={<Catalog/>}
+            />
+            <Route
+                path='/'
+                exact
+                element={<Home/>}
+            />                    
+        </Routes>
+        <Footer/>
+        </>
+    );
+}
+
+export default App;
